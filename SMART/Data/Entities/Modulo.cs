d6b743851f73b977312fa32944e49732ae190e9b
Charles Michel Laurindo de Leão <charles.leao@beeit.com.br>
2024-05-14 16:47:28 -0300

    Adiciona libs de análise estática de códigos

--- SMART/Data/Entities/Modulo.cs
+++ SMART/Data/Entities/Modulo.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
 
         public Guid CursoId { get; set; }
-        
+
         public int Numero {get; set; }
 
         public int CargaHoraria { get; set; }
